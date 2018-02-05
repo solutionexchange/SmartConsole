bf75ef50114721b61688a9f2084e58ff9e331c3c
Thomas Pollinger <thomas.pollinger@vodafone.com>
2018-02-05 17:32:46 +0100

    New script for listing projects

---
+++ Scripts/Projects/Get-AllProjects.ps1
@@ -0,0 +1,18 @@
+﻿Register-MSConfigStore;
+
+Set-MSConfigDebugMode -Value ($false); # $true oder $false
+
+Register-MSSession -UseDefaults ($true);
+Select-MSSession -UseDefaults ($true);
+Enter-MSSession -UseDefaults ($true);
+
+Get-MSAllProjects;
+$AllProjects = ([xml](Get-MSSessionProperty -Name ("LastResponse"))).SelectNodes("IODATA/PROJECTS/PROJECT");
+
+$AllProjects;
+
+#Show-MSSession; # Optional
+Exit-MSSession -UseDefaults ($true);
+Unregister-MSSession -UseDefaults ($true);
+
+Unregister-MSConfigStore;
