944beac0be490a41986a5456603cb39f8ec90d41
Thomas Pollinger <thomas.pollinger@vodafone.com>
2018-06-08 15:41:07 +0200

    Update for 0.2.8

---
+++ Private/Tools/ConvertTo-OADate.ps1
@@ -0,0 +1,40 @@
+﻿Function ConvertTo-OADate {
+    <#
+    .SYNOPSIS
+        ... {SYNOPSIS} ...
+    .DESCRIPTION
+        ... {DESCRIPTION} ...
+    .EXAMPLE
+        ... {EXAMPLE} ...
+    .INPUTS
+        ... {INPUTS} ...
+    .OUTPUTS
+        ... {OUTPUTS} ...
+    .LINK
+        ... {LINK} ...
+    .NOTES
+        ... {NOTES} ...
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'byOLEAutomationDate')]
+    param(
+        # {DESCRIPTION}
+        [Parameter(
+            Position = 0,
+            Mandatory = $true,
+            ParameterSetName = 'byOLEAutomationDate',
+            ValueFromPipeline = $true,
+            ValueFromRemainingArguments = $true
+        )]
+        [datetime] $Date
+    )
+    begin {
+        Write-Debug -Message ("[ Enter => function {0} ]" -f $MyInvocation.MyCommand);
+    }
+    process {
+        Write-Debug -Message ("[ Process => function {0} ]" -f $MyInvocation.MyCommand);
+        return $Date.ToOADate();
+    }
+    end {
+        Write-Debug -Message ("[ Leave => function {0} ]" -f $MyInvocation.MyCommand);
+    }
+}
